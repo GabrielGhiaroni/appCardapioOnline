4f32788cbe2fd45c69a21c1048f7a515a3ff7550
Gabriel Ghiaroni <gabrielghiaroni@outlook.com>
2024-01-10 17:56:30 -0300

    criacao Classe Context (AppDbContext) e instalacaodo Entity FrameWork

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace appCardapioOnline.Context {
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+
+        }
+        public DbSet<Categoria> Categorias {get; set;}
+        public DbSet<Lanche> Lanches {get; set;}
+    }
+}
