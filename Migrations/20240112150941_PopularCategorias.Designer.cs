3879de7ea3138053cf030540262a93412bce1312
Gabriel Ghiaroni <gabrielghiaroni@outlook.com>
2024-01-12 12:39:23 -0300

    tabela Categorias populada

--- Migrations/20240112150941_PopularCategorias.Designer.cs
+++ Migrations/20240112150941_PopularCategorias.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using appCardapioOnline.Context;
 
@@ -10,9 +11,11 @@ using appCardapioOnline.Context;
 namespace appCardapioOnline.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240112150941_PopularCategorias")]
+    partial class PopularCategorias
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240112150941_PopularCategorias.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace appCardapioOnline.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) " +
+            "VALUES('Vegetariano', 'Lanches feitos a base de carne de soja.')"
+            );
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) " +
+            "VALUES('Carne Bovina', 'Lanches feitos com carnes de primeira linha.')"
+            );
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) " +
+            "VALUES('Natural', 'Lanches saudáveis para um dia mais leve.')"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
